082d4efdf693a145202bf927eee595a9175c6d7a
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-09 15:49:18 -0800

    added html reporters

--- pytest/tests/test_suite_1.py
+++ pytest/tests/test_suite_1.py
@@ -1,13 +1,24 @@
 import pytest
 
-pytestmark = [pytest.mark.fe, pytest.mark.slow]
+pytestmark = [pytest.mark.be, pytest.mark.slow]
+
+@pytest.fixture(scope='module')
+def setup():
+    print("")
+    print(">>> SETUP <<<")
+
+    return {'id': 20, 'name': 'Tarik'}
 
 @pytest.mark.smoke
-def test_login_page_valid_user():
+@pytest.mark.ll
+def test_login_page_valid_user(setup):
     print("Login with valid user")
     print("Function: test1")
+    print("Name: {}".format(setup.get('name')))
+    # import pdb; pdb.set_trace()  # break point
+
 
 @pytest.mark.regression
-def test_login_page_wrong_password():
+def test_login_page_wrong_password(setup):
     print("Login with wrong user")
     print("Function: test2")
--- pytest/tests/test_suite_2.py
+++ pytest/tests/test_suite_2.py
@@ -2,13 +2,20 @@ import pytest
 
 pytestmark = [pytest.mark.fe, pytest.mark.slow]
 
-@pytest.mark.smoke
+@pytest.fixture(scope='module')
+def setup():
+    print("")
+    print(">>> SETUP <<<")
+
+    return {'id': 20, 'name': 'Tarik'}
+
+@pytest.mark.abc
 class TestCheckout(object):
 
-    def test_checkout_as_guest(self):
+    def test_checkout_as_guest(self, setup):
         print("Checkout as guest")
         print("Function: Test11")
 
-    def test_checkout_with_existing_user(self):
+    def test_checkout_with_existing_user(self, setup):
         print("Checkout with existing user")
         print("Function: test")
