b4fbe2f368709cdd20d1258d081448dfd581b883
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-10 20:09:27 -0800

    framework setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='api_tests',
+      version='1.0',
+      description='API Automation',
+      author='Tarik Deliktas',
+      author_email='tarikdeliktas@gmail.com',
+      url='https://github.com/tarikdelikktas',
+      packages=find_packages()
+      )
