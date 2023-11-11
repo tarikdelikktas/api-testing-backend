1b9d123f6397f2d1550c937fe913fbcd6cd57716
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-11 00:36:03 -0800

    created helper functions for generic utulities

--- api_tests/tests/customers/test_create_customers_smoke.py
+++ api_tests/tests/customers/test_create_customers_smoke.py
@@ -1,14 +1,17 @@
 import pytest
 import logging as logger
+from api_tests.src.utilities.genericUtilities import generate_random_email_and_password
 
 @pytest.mark.tcid1
 def test_create_customer_only_email_password():
     logger.info("TEST: Create new customer with email and password only")
 
-    email = ''
-    password = ''
+    rand_info = generate_random_email_and_password()
+    email = rand_info['email']
+    password = rand_info['password']
 
     # create payload
+    payload = {'email': email, 'password': password}
 
     # make the call
 
---
+++ build/lib/api_tests/tests/test_healthcheck.py
@@ -0,0 +1,3 @@
+import logging as logger
+def test_healthcheck_1():
+    logger.info("Just running healthcheck 1.")
