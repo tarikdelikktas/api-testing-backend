75580ed7d80d51b78f93b0e50fa34ee73d00649a
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-12 12:40:48 -0800

    customer object added for payload

--- api_tests/src/helpers/customers_helper.py
+++ api_tests/src/helpers/customers_helper.py
@@ -16,5 +16,4 @@ class CustomerHelper(object):
         payload['password'] = password
         payload.update(kwargs)
 
-
-
+        return True
--- api_tests/tests/customers/test_create_customers_smoke.py
+++ api_tests/tests/customers/test_create_customers_smoke.py
@@ -1,6 +1,9 @@
+import pdb
+
 import pytest
 import logging as logger
 from api_tests.src.utilities.genericUtilities import generate_random_email_and_password
+from api_tests.src.helpers.customers_helper import CustomerHelper
 
 @pytest.mark.tcid1
 def test_create_customer_only_email_password():
@@ -10,11 +13,11 @@ def test_create_customer_only_email_password():
     email = rand_info['email']
     password = rand_info['password']
 
-    # create payload
-    payload = {'email': email, 'password': password}
-
     # make the call
+    cust_obj = CustomerHelper()
+    cust_api_info = cust_obj.create_customer(email=email, password=password)
 
+    import pdb; pdb.set_trace()
     # verify status code of the call
 
     # verify email in the response
