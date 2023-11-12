fe2a393d8b3c1d554bf85d38a4856ab3b2a1998c
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-12 13:13:00 -0800

    API authentication handle by OAuth1.0

--- api_tests/src/utilities/requestsUtility.py
+++ api_tests/src/utilities/requestsUtility.py
@@ -1,23 +1,26 @@
 from api_tests.src.configs.hosts_config import API_HOSTS
 import requests
 import os
+import json
+from requests_oauthlib import OAuth1
 
 
 class RequestsUtility(object):
     def __init__(self):
         self.env = os.environ.get('ENV', 'test')
         self.base_url = API_HOSTS[self.env]
+        self.auth = OAuth1("ck_67ac45d287c3e936affb782c099203d06de749ac", "cs_c34948a934b1935f3ea94b08d311cba0e13362e4")
 
-    def post(self, endpoint, payload=None, headers=None):
+    def post(self, endpoint, payload=None, headers=None, expected_status_code=200):
         if not headers:
             headers = {"Content-Type": "application/json"}
 
         url = self.base_url + endpoint
-        import json
-        rs_api = requests.post(url=url, data=json.dumps(payload), headers=headers)
 
-        import pdb;
-        pdb.set_trace()
+        rs_api = requests.post(url=url, data=json.dumps(payload), headers=headers, auth=self.auth)
+        self.status_code = rs_api.status_code
+        assert self.status_code == int(expected_status_code), f'Expected status code {expected_status_code}, but actual {self.status_code}'
+        return rs_api.json()
 
     def get(self):
         pass
