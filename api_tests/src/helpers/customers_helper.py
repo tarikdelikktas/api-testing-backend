373c10fa5c80fe62ca4c4b15cc5e2bb06e495894
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-12 12:36:44 -0800

    created helper class for email/pswd generation

---
+++ api_tests/src/helpers/customers_helper.py
@@ -0,0 +1,20 @@
+from api_tests.src.utilities.genericUtilities import  generate_random_email_and_password
+class CustomerHelper(object):
+    def __init__(self):
+        pass
+
+    def create_customer(self, email=None, password=None, **kwargs):
+
+        if not email:
+            ep = generate_random_email_and_password()
+            email = ep['email']
+        if not password:
+            password = 'Password1'
+
+        payload = dict()
+        payload['email'] = email
+        payload['password'] = password
+        payload.update(kwargs)
+
+
+
