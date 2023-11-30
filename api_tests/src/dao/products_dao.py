eae783761d997d5749392e95c96074649a6bf391
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-11-30 14:26:41 +0300

    get lists products with db verification

--- api_tests/src/dao/products_dao.py
+++ api_tests/src/dao/products_dao.py
@@ -7,10 +7,20 @@ class ProductsDAO(object):
         self.db_helper = DBUtility()
 
     def get_random_product_from_db(self, qty=1):
+
         sql = 'SELECT * FROM local.wp_posts WHERE post_type = "product" LIMIT 5000;'
         rs_sql = self.db_helper.execute_select(sql)
+
         return random.sample(rs_sql, int(qty))
 
     def get_product_by_id(self, product_id):
+
         sql = f'SELECT * FROM local.wp_posts WHERE ID = {product_id};'
+
+        return self.db_helper.execute_select(sql)
+
+    def get_products_created_after_given_date(self, _date):
+
+        sql = f'SELECT * FROM local.wp_posts WHERE post_type = "product" AND post_date > "{_date}" LIMIT 1000;'
+
         return self.db_helper.execute_select(sql)
--- api_tests/tests/products/test_get_products_by_filter.py
+++ api_tests/tests/products/test_get_products_by_filter.py
@@ -1,4 +1,5 @@
 from api_tests.src.helpers.products_helper import ProductsHelper
+from api_tests.src.dao.products_dao import ProductsDAO
 import pytest
 from datetime import datetime, timedelta
 import pdb
@@ -14,6 +15,7 @@ class TestProductsWithFilter(object):
         _after_created_date = datetime.now().replace(microsecond=0) - timedelta(days=x_days_from_today)
         after_created_date = _after_created_date.isoformat()
 
+
         # Another way to create a date
         # tmp_date = datetime.now() - timedelta(days=x_days_from_today)
         # after_created_date = tmp_date.strftime('%Y-%M-%dT%H:%m:%S')
@@ -21,9 +23,19 @@ class TestProductsWithFilter(object):
         # make the API call
         payload = dict()
         payload['after'] = after_created_date
+        payload['per_page'] = 100
         rs_api = ProductsHelper().call_list_all_products(payload)
         assert rs_api, f"Empty Response for 'list products with filter"
-        pdb.set_trace()
 
         # get the data from DB
+        db_products = ProductsDAO().get_products_created_after_given_date(after_created_date)
+
         # verify response
+        assert len(rs_api) == len(db_products), (f"List products with filters 'after' returned unexpected numberof products." \
+                                                 f"Expected: {len(db_products)}, Actual: {len(rs_api)}")
+
+        ids_in_api = [i['id'] for i in rs_api]
+        ids_in_db = [i['ID'] for i in db_products]
+
+        ids_diff = list(set(ids_in_api) - set(ids_in_db))
+        assert not ids_diff, f"List products with filter, product ids in response mismatch in db."
